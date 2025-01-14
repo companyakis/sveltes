56ae4fc42aee1201d560b9a01333a05ea52bdc2b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-14 10:46:00 +0300

    Create 0003-components2-HeaderArea.svelte

---
+++ 0003-components2-HeaderArea.svelte
@@ -0,0 +1,48 @@
+<script>
+
+</script>
+
+<div class="header-area">
+
+    <div class="header-title"><h1>Svelte 2025</h1></div>
+
+    <div class="links">
+        <a class="link" href="">Why Svelte</a>
+        <a class="link" href="">Learn Svelte</a>
+        <a class="link" href="">Faq</a>
+    </div>
+
+</div>
+
+<style>
+  
+  .header-area {
+    height: 70px;
+    background-color: purple;
+    color: #000;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .header-title {
+
+    color: bisque;
+    margin-left: 10px;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+  }
+
+  .links {
+
+    margin-right: 10px;
+  }
+
+  .link {
+
+    text-decoration: none;
+    margin-right: 5px;
+    font-size: 20px;
+    color: bisque;
+  }
+
+</style>
