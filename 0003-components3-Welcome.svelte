960c117cd8c0303a4a317a2eef1237ee900145c7
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-14 10:46:35 +0300

    Create 0003-components3-Welcome.svelte

---
+++ 0003-components3-Welcome.svelte
@@ -0,0 +1,30 @@
+<script>
+    
+    let welcomeProverb = "Welcome! A rolling stone gathers no moss..."
+
+</script>
+
+<div class="welcome-area">
+
+    <p>{welcomeProverb}</p>
+
+    <p>Let's learn and use Svelte in 2025!</p>
+
+</div>
+
+<style>
+
+    .welcome-area {
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin-top: 150px;
+        font-size: 30px;
+        color: blueviolet;
+       
+    }
+
+
+</style>
