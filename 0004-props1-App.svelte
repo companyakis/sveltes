46fd6eacd554064a0d3ee5311567c1fd137bc056
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-14 23:55:05 +0300

    Create 0004-props1-App.svelte

---
+++ 0004-props1-App.svelte
@@ -0,0 +1,33 @@
+<script>
+
+  import CounterScreen from "./components/CounterScreen.svelte";
+
+  let counter = 0
+
+  function counterUp() {
+    counter++
+  }
+
+  function counterDown() {
+    counter--
+  }
+
+</script>
+
+<main>
+
+  <CounterScreen {counter} />
+
+  <div>
+    <button on:click={counterUp}>Counter ++</button>
+
+    <button on:click={counterDown}>Counter --</button>
+  </div>
+
+</main>
+
+
+<style>
+
+
+</style>
