bf68d2def8a93c58d8e6ee4f30ababc71e7f06e4
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-15 00:22:00 +0300

    Create 0005-spread-props2-ProductCard.svelte

---
+++ 0005-spread-props2-ProductCard.svelte
@@ -0,0 +1,35 @@
+<script>
+    export let product
+    export let price
+    export let explanation
+    export let creator
+</script>
+
+<div class="card">
+
+    <h1>Product name: {product}</h1>
+
+    <h3>Price: {price}</h3>
+
+    <h4>Product info: {explanation}</h4>
+
+    <h3>Product owner: {creator}</h3>
+
+</div>
+
+<style>
+
+    .card {
+
+        width: 500px;
+        height: 200px;
+        border: none;
+        background-color: blueviolet;
+        color: rgb(226, 233, 233);
+        text-align: center;
+        padding: 10px;
+        border-radius: 10px;
+    }
+
+
+</style>
