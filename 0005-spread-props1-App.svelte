e3eef97fbc7f56eb885ac7d0e7ee2682c6ae9a0e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-15 00:21:08 +0300

    Create 0005-spread-props1-App.svelte

---
+++ 0005-spread-props1-App.svelte
@@ -0,0 +1,24 @@
+<script>
+
+  import ProductCard from "./components/ProductCard.svelte";
+
+  const productInfo = {
+    product: "AI App",
+    price: "5000 ₺",
+    explanation: "AI app for Web3 dev",
+    creator: "Mustafa Büyükdereli"
+  }
+
+</script>
+
+<main>
+
+  <ProductCard {...productInfo} />
+ 
+</main>
+
+
+<style>
+
+
+</style>
