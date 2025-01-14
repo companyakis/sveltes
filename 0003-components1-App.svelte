d18ceae99f91093af0494f7b4a16f90d720222ec
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-14 10:45:04 +0300

    Create 0003-components1-App.svelte

---
+++ 0003-components1-App.svelte
@@ -0,0 +1,19 @@
+<script>
+
+  import HeaderArea from "./components/HeaderArea.svelte";
+  import Welcome from "./components/Welcome.svelte";
+
+</script>
+
+<main>
+
+  <HeaderArea />
+  <Welcome />
+
+</main>
+
+
+<style>
+
+
+</style>
