46c0df5a64008230d0aeff85adf9783a76d98851
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-14 10:01:26 +0300

    Create 0002-store-year5-Yearup.svelte

---
+++ 0002-store-year5-Yearup.svelte
@@ -0,0 +1,13 @@
+<script>
+
+    import { year } from "./store";
+
+    function yearPlusOne() {
+
+        year.update(y => y + 1)
+    }
+
+</script>
+
+
+<button on:click={yearPlusOne}>Year ++</button>
