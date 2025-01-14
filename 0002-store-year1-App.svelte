92c546d9cf82a140dec80c41b464c85d48c03ffe
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-14 09:58:45 +0300

    Create 0002-store-year1-App.svelte

---
+++ 0002-store-year1-App.svelte
@@ -0,0 +1,32 @@
+<script>
+
+  import Yearup from "./Yearup.svelte";
+  import Yeardown from "./Yeardown.svelte";
+  import Yearreset from "./Yearreset.svelte";
+
+  import { year } from "./store";
+
+</script>
+
+<main>
+
+<div class="year-info">Year: {$year}</div>
+
+<div>
+  <Yearup />
+  <Yeardown />
+  <Yearreset />
+</div>
+
+</main>
+
+<style>
+
+  .year-info {
+    font-size: 35px;
+    margin-bottom: 10px;
+    color: purple;
+  }
+
+
+</style>
